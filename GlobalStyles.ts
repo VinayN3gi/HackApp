b56891abf8042c71fba0c193757feb9f0f5e29c8
Vinay Negi <vinegi004@gmail.com>
2024-04-06 19:02:13 +0530

    Add main screen

---
+++ GlobalStyles.ts
@@ -0,0 +1,22 @@
+/* fonts */
+export const FontFamily = {
+    interSemiBold: "Inter-SemiBold",
+    interMedium: "Inter-Medium",
+    poppinsBold: "Poppins-Bold",
+    poppinsRegular: "Poppins-Regular",
+    poppinsSemiBold: "Poppins-SemiBold",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_base: 16,
+  };
+  /* Colors */
+  export const Color = {
+    colorWhite: "#fff",
+    colorDodgerblue: "#407ce2",
+  };
+  /* border radiuses */
+  export const Border = {
+    br_13xl: 32,
+  };
+  
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,176 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from "react";
+import { Image } from "expo-image";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Border, FontSize, FontFamily, Color } from "../GlobalStyles"
 
-const index = () => {
+const Onboarding = () => {
   return (
-    <View>
-      <Text className=' text-red-400'>index</Text>
+    <View style={styles.onboarding3}>
+      <View className=" justify-center items-center mt-24">
+        <Image className="  h-40 w-40 "
+        contentFit="cover"
+        source={require("../assets/Logo.png")}
+        />
+      </View>
+      <View className=" justify-center items-center mt-24">
+        <Text className=" text-3xl font-medium">Log in to get started</Text>
+      </View>
+      <View className=" justify-center items-center mt-5">
+        <Text className=" text-xl font-light">Login to get all your medical</Text>
+        <Text className=" text-xl font-light">records in one place</Text>
+      </View>
+      <View className=" justify-center items-center  mt-24">
+      <TouchableOpacity className=" bg-blue-400 w-[75%] h-14 rounded-full justify-center items-center">
+        <Text className=" text-white font-semibold text-xl">Sign in</Text>
+      </TouchableOpacity>
+      </View>
+      <View className=" justify-center items-center mt-6">
+        <TouchableOpacity className=" bg-white w-[75%] h-14 rounded-full justify-center items-center border-blue-400 border-2">
+          <Text className=" font-semibold text-xl text-blue-400">Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default index
+const styles = StyleSheet.create({
+  parentLayout: {
+    width: 300,
+    position: "absolute",
+  },
+  rectangleParentLayout: {
+    height: 56,
+    left: 0,
+  },
+  groupLayout: {
+    width: 250,
+    position: "absolute",
+  },
+  framePosition: {
+    borderRadius: Border.br_13xl,
+    bottom: "0%",
+    right: "0%",
+    height: "100%",
+    left: "0%",
+    top: "0%",
+    position: "absolute",
+    width: "100%",
+  },
+  loginPosition: {
+    top: "28.57%",
+    height: "42.86%",
+    lineHeight: 24,
+    fontSize: FontSize.size_base,
+    textAlign: "center",
+    position: "absolute",
+  },
+  vectorIcon: {
+    height: "69.54%",
+    width: "64.13%",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    overflow: "hidden",
+  },
+  cloudcare: {
+    top: 113,
+    fontSize: 25,
+    fontFamily: FontFamily.poppinsSemiBold,
+    color: "#223a6a",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    textAlign: "center",
+    fontWeight: "600",
+    width: 184,
+    left: 58,
+    position: "absolute",
+  },
+  vectorParent: {
+    height: 151,
+    width: 184,
+  },
+  loginToStay: {
+    top: 32,
+    letterSpacing: 1,
+    fontFamily: FontFamily.poppinsRegular,
+    color: "rgba(34, 31, 31, 0.6)",
+    lineHeight: 24,
+    fontSize: FontSize.size_base,
+    left: 0,
+    textAlign: "center",
+    width: 300,
+    position: "absolute",
+  },
+  headline: {
+    fontSize: 22,
+    lineHeight: 30,
+    fontWeight: "700",
+    color: "#221f1f",
+    left: "0%",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    textAlign: "center",
+    top: "0%",
+    position: "absolute",
+    width: "100%",
+  },
+  loginToStayHealthyAndFitParent: {
+    top: 176,
+    width: 300,
+    position: "absolute",
+  },
+  frameChild: {
+    borderStyle: "solid",
+    borderColor: Color.colorDodgerblue,
+    borderWidth: 1,
+  },
+  signUp: {
+    width: "24%",
+    left: "38%",
+    fontWeight: "500",
+    fontFamily: FontFamily.interMedium,
+    color: Color.colorDodgerblue,
+  },
+  rectangleParent: {
+    top: 72,
+    height: 56,
+    left: 0,
+  },
+  frameItem: {
+    backgroundColor: Color.colorDodgerblue,
+  },
+  login: {
+    width: "17.2%",
+    left: "41.6%",
+    fontFamily: FontFamily.interSemiBold,
+    color: Color.colorWhite,
+    fontWeight: "600",
+    height: "42.86%",
+  },
+  rectangleGroup: {
+    height: 56,
+    left: 0,
+    top: 0,
+    width: 250,
+  },
+  frameGroup: {
+    top: 274,
+    left: 25,
+    height: 128,
+  },
+  frameParent: {
+    top: 199,
+    left: 30,
+    height: 402,
+  },
+  onboarding3: {
+    backgroundColor: Color.colorWhite,
+    flex: 1,
+    height: 800,
+    overflow: "hidden",
+    width: "100%",
+  },
+});
+
+export default Onboarding;
